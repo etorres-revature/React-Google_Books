d97741448848244058a6be3d8822f00553848e6e
Eric D. Torres <etorresnotary@gmail.com>
2020-12-16 10:17:34 -0600

    updates to server/update package json with scripts

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require("express")
-const logger = require("morgan")
-const mongoose = require("mongoose")
-const compression = require("compression")
-const dotenv = require("dotenv").config()
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const compression = require("compression");
+const dotenv = require("dotenv").config();
 
-const PORT = process.env.PORT || 59874
+const PORT = process.env.PORT || 59874;
 
 const app = express();
 
-app.use(logger("dev"))
-app.use(compression())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/build"));
+}
+
+app.use(require(routes));
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
-  // routes
-  app.use(require("./routes/api.js"));
-  
-  app.listen(PORT, () => {
-    console.log(`App is active at http://localhost:${PORT}`);
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App is active at http://localhost:${PORT}`);
+});
